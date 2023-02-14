23cd4c501882c2cdf2f27560b957ff756512a24b
corentin jezierski <corentin.jezierski@gmail.com>
2023-02-14 22:04:44 +0100

    systemes de routes fonctionnel

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Psycoco-d-lik
 Mon catalogue de projets 3D
+
+```
+npm i
+```
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express'
 
-
 const app = express();
 const port = 3000;
 
+// View engine setup
+app.set('view engine', 'ejs');
+
+app.use(express.static('src'))
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+
+app.get('/hello', function(req, res){
+  res.render('hello');
 });
 
 app.listen(port, () => {
